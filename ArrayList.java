c5b120a6463d79e3125e628fc84fd206c132df29
Suyashri Satappa Patil <suyashripatil@gmail.com>
2026-05-10 13:50:03 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+class ArrayList
+{
+    public static void main(String args[])
+    {
+        
+        ArrayList<Integer> list=new ArrayList<Integer>();
+      list.add(0);
+      list.add(2);
+      list.add(3);
+
+      System.out.println(list);
+
+
+    }
+}
